42f02ed15ee9757b156836d14f0cf37a76a168fa
Dominic <dominiclopez391@gmail.com>
2018-02-17 15:18:00 -0800

    pid tweaks

--- src/frc/team6612/Robot.java
+++ src/frc/team6612/Robot.java
@@ -10,6 +10,7 @@ package frc.team6612;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.kauailabs.navx.frc.*;
+import edu.wpi.first.wpilibj.hal.I2CJNI;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -20,7 +21,7 @@ public class Robot extends IterativeRobot implements PIDOutput {
     private AHRS sensor; //the sensor pulling data from the robot
     private PIDController pid; //does calculations to make accurate turns
     private Encoder lEncoder, rEncoder; //wheel
-    private SerialPort arduino;
+    private I2C arduino;
 
     private DifferentialDrive myRobot; //"tank drive"
     private Joystick controller;
@@ -31,17 +32,18 @@ public class Robot extends IterativeRobot implements PIDOutput {
     private Spark winch;
     private Compressor compressor;
     private Solenoid solenoid1, solenoid2; //pneumatic control for cube launch
-    private double kP = 0.05, kI = 0.0, kD = 0.1, kF = 0;
+    private double kP = 0.045, kI = 0.0, kD = 0.1, kF = 0;
 
     //.045 0 .1 0
     //Old Robot PID Constants: .025, 0, 0.1, 0
     private byte[] distance;
-    private final double INCHES_PER_ENCODER_PULSE = 0.942477796;
+    private final float INCHES_PER_ENCODER_PULSE = 0.942477796f / 10.75f;
 
     @Override
     public void robotInit() {
 
         //Objects Initialization :-)
+        distance = new byte[32];
         winch = new Spark(1);
         sensor = new AHRS(I2C.Port.kMXP);
         pid = new PIDController(kP, kI, kD, kF, sensor, this);
@@ -52,11 +54,11 @@ public class Robot extends IterativeRobot implements PIDOutput {
         compressor = new Compressor(0);
         solenoid1 = new Solenoid(0);
         solenoid2 = new Solenoid(1);
-        //arduino = new SerialPort(115200, SerialPort.Port.kUSB);
+        arduino = new I2C(I2C.Port.kOnboard,0);
 
 
         //Variable Settings
-        pid.setOutputRange(-0.5, 0.5);
+        pid.setOutputRange(-0.45, 0.45);
         pid.setInputRange(-1080, 1080);
         pid.setAbsoluteTolerance(1); //min. degree that pid can read. If it's within (1) degree, returns pid.onTarget() as true
         compressor.setClosedLoopControl(true); //compressor auto-regulates its pressure
@@ -97,12 +99,13 @@ public class Robot extends IterativeRobot implements PIDOutput {
     @Override
     public void teleopPeriodic() {
 
-        //System.out.println(new String(arduino.read(arduino.getBytesReceived())));
+
         motorController();
         driveControl();
         //System.out.println(lEncoder.getDistance() + " " + rEncoder.getDistance());
         pistonControl();
         //Methods for Forklift, Claw
+        printDistance();
 
 
     }
@@ -124,13 +127,17 @@ public class Robot extends IterativeRobot implements PIDOutput {
         t.start();
 
         Timer.delay(3);
-        myRobot.arcadeDrive(0, 0);*/
+        myRobot.arcadeDrive(0, 0);
 
 
         autonomousEnabled = true;
         sensor.reset();
-        turnAngle(90, 60);
+        turnAngle(90, 2);
         System.out.println(sensor.getAngle() + " " + pid.getSetpoint());
+        */
+
+        driveDistance(24);
+
     }
 
     @Override
@@ -161,6 +168,7 @@ public class Robot extends IterativeRobot implements PIDOutput {
             case 1:
                 if (plateColors.charAt(0) == 'L') {
                     System.out.println("drive to left side of switch past auto line, place power cube on");
+                    //start on leftmost side of diamond plate, drive 168 inches forward (middle switch
                 } else {
                     System.out.println("drive to right side of switch past auto line, place power cube on");
                 }
@@ -200,6 +208,38 @@ public class Robot extends IterativeRobot implements PIDOutput {
 
         solenoid1.set(soleOnePowered);
         solenoid2.set(soleTwoPowered);
+
+    }
+
+    private void driveDistance(float distanceInches) {
+
+        double currentDistance = 0;
+        double speed = 0;
+        double adjustedSpeed = 0;
+
+        while(currentDistance < distanceInches) {
+
+            if(lEncoder.getDistance() <= rEncoder.getDistance()) {
+                currentDistance = rEncoder.getDistance() * INCHES_PER_ENCODER_PULSE;
+                speed = (distanceInches - currentDistance) / distanceInches;
+            }
+            else {
+                currentDistance = lEncoder.getDistance() * INCHES_PER_ENCODER_PULSE;
+                speed = (distanceInches - currentDistance) / distanceInches;
+            }
+
+            if(speed > 0)
+                adjustedSpeed = speed + MIN_ROTATIONSPEED - (speed * MIN_ROTATIONSPEED);
+            else
+                adjustedSpeed = speed - MIN_ROTATIONSPEED - (speed * MIN_ROTATIONSPEED);
+
+            myRobot.arcadeDrive(adjustedSpeed, 0);
+
+        }
+
+
+
+        myRobot.arcadeDrive(0,0);
 
     }
 
@@ -263,6 +303,10 @@ public class Robot extends IterativeRobot implements PIDOutput {
 
         pid.disable();
 
+    }
+    void printDistance(){
+        arduino.read(8,1,distance);
+        System.out.println(distance[0]);
     }
 
 }
